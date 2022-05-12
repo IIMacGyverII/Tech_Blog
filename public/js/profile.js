3615f1a70422880d70f2f60c459fd1bb569b2e1c
Joshua Christianson <macgyverjunk@yahoo.com>
2022-05-12 18:17:47 -0500

    new post working

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,32 +1,8 @@
-const newFormHandler = async (event) => {
-  event.preventDefault();
-
-  const username = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
-
-  if (username && needed_funding && description) {
-    const response = await fetch(`/api/projects`, {
-      method: 'POST',
-      body: JSON.stringify({ username, needed_funding, description }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to create project');
-    }
-  }
-};
-
 const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/projects/${id}`, {
+    const response = await fetch(`/api/post/${id}`, {
       method: 'DELETE',
     });
 
@@ -38,10 +14,13 @@ const delButtonHandler = async (event) => {
   }
 };
 
-document
-  .querySelector('.new-project-form')
-  .addEventListener('submit', newFormHandler);
+function initializedEventListeners() {
+  let deleteButtons = document.querySelectorAll('.deletePostBtn');
 
-document
-  .querySelector('.project-list')
-  .addEventListener('click', delButtonHandler);
+  for (let i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].addEventListener("click", delButtonHandler);
+  }
+
+}
+
+initializedEventListeners();
