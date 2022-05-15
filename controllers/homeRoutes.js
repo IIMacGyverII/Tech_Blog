5b651f76d5f097c6905902b05414f891291e9cd3
Joshua Christianson <macgyverjunk@yahoo.com>
2022-05-14 23:23:18 -0500

    newcomment route made

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -142,6 +142,31 @@ router.get('/post/:id/edit', withAuth, async (req, res) => {
   }
 });
 
+router.get('/newcomment', async (req, res) => {
+  try {
+    // Get all comments and JOIN with user data
+    const commentData = await Comment.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const comments = commentData.map((comment) => comment.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('newcomment', { 
+      comments, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
